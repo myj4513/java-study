6b0b1db95cfe0b841525cd8f983777e358dfc19f
myj4513 <yejungod@gmail.com>
2021-07-24 21:54:16 +0900

    ~chap15/practice16

---
+++ src/chap15/practice16/FileReaderEx1.java
@@ -0,0 +1,28 @@
+package chap15.practice16;
+
+import java.io.*;
+
+public class FileReaderEx1 {
+    public static void main(String[] args) {
+        try{
+            String fileName = "text.txt";
+            FileInputStream fis = new FileInputStream(fileName);
+            FileReader fr = new FileReader(fileName);
+
+            int data = 0;
+            while((data = fis.read())!= -1){
+                System.out.print((char)data);
+            }
+            System.out.println();
+            fis.close();
+
+            while((data = fr.read())!=-1){
+                System.out.print((char)data);
+            }
+            System.out.println();
+            fr.close();
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+}
